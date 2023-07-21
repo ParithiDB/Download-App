b0abe80bd8f194d424bc62c56c29fc1ddf859e6c
ParithiDB <samu.aidindia@gmail.com>
2023-07-21 19:43:21 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const urlInput = document.querySelector("input");
+const downloadBtn = document.querySelector("button");
+
+downloadBtn.addEventListener("click", async () => {
+    try {
+            const response = await fetch(urlInput.value);
+            const file = await response.blob();
+            const link = document.createElement("a");
+            link.href = URL.createObjectURL(file);
+            link.download = new Date().getTime();
+            link.click();
+            urlInput.value = '';
+
+    } catch (error) {
+
+            alert("Failed to Download","1. Check Internet connection", "2. Check whether the URL is Valid!")
+    }
+});
